d177a242cff65c4395789432b7a7abebac98f7ef
Harshal Patil <harshalpatil2012@gmail.com>
2023-10-02 11:49:47 +0530

    Redis embedded example

---
+++ Embedded-Redis/src/main/java/org/example/RedissonConfig.java
@@ -0,0 +1,20 @@
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379")
+                .setDatabase(0);
+        return Redisson.create(config);
+    }
+}
