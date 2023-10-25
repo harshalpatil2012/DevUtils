79619da23d9e015040449c5948fbfbef18eebab4
Harshal Patil <harshalpatil2012@gmail.com>
2023-10-25 08:23:46 +0530

    Update Spring-Http-Interceptor.java

--- SpringBoot-App-Deployment/Spring-Http-Interceptor.java
+++ SpringBoot-App-Deployment/Spring-Http-Interceptor.java
@@ -1,183 +1,309 @@
-Interceptor for http request and response
+package com.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Map;
-
-public class RequestInterceptor implements HandlerInterceptor {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RequestInterceptor.class);
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logRequest(request);
-
-        // Update the request URL path
-        String newPath = request.getPathInfo() + "/updated";
-        request.setAttribute(HttpServletRequest.REQUEST_URI, newPath);
-
-        // Update the request parameters
-        Map<String, String[]> params = request.getParameterMap();
-        params.put("updatedParam", new String[]{"updatedValue"});
-
-        // Update the request payload
-        String requestBody = IOUtils.toString(request.getInputStream());
-
-        // Modify the request body as needed
-        requestBody = requestBody.replace("old", "new");
-
-        // Wrap the request object
-        HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper(request);
-        requestWrapper.setContent(requestBody.getBytes());
-        request = requestWrapper;
-
-        return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-    }
-
-    private void logRequest(HttpServletRequest request) throws IOException {
-        String requestBody = IOUtils.toString(request.getInputStream());
-
-        LOGGER.info("Request: {} {} {} {}", request.getMethod(), request.getRequestURI(), request.getContentType(), requestBody);
-    }
-}
-
-public class ResponseInterceptor implements HandlerInterceptor {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ResponseInterceptor.class);
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        byte[] responseBody = IOUtils.toByteArray(response.getOutputStream());
-
-        // Modify the response body as needed
-        responseBody = responseBody.replace("old", "new");
-
-        // Wrap the response object
-        HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(response);
-        responseWrapper.setContent(responseBody);
-        response = responseWrapper;
-
-        LOGGER.info("Response: {} {} {} {}", response.getStatus(), response.getContentType(), response.getHeader("Content-Length"), responseBody);
-    }
-}
-
-
-
-Using reacitive Spring:
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.reactive.function.server.HandlerFilterFunction;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-public class ReactiveRequestInterceptor implements HandlerFilterFunction<ServerRequest, ServerResponse> {
+public interface ReactiveResponseInterceptor {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ReactiveRequestInterceptor.class);
+    Mono<ServerWebExchange> filter(ServerWebExchange exchange, WebFilterChain chain);
+}
 
-    // Update the request URL path
-        String newPath = request.uri() + "/updated";
-        ServerRequest updatedRequest = ServerRequest.create(request.exchange(), newPath);
+package com.example;
 
-        // Update the request parameters
-        updatedRequest = updatedRequest.queryParam("updatedParam", "updatedValue");
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public interface ReactiveRequestInterceptor {
+
+    Mono<ServerWebExchange> filter(ServerWebExchange exchange, WebFilterChain chain);
+}
+
+package com.example;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.reactive.filter.WebFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class WebFluxConfig {
+
+    @Bean
+    public WebFilter requestResponseFilter(ReactiveRequestInterceptor requestInterceptor, ReactiveResponseInterceptor responseInterceptor) {
+        return new WebFilter() {
+
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+                return requestInterceptor.filter(exchange, chain)
+                        .then(responseInterceptor.filter(exchange, chain));
+            }
+        };
+    }
+
+    @Bean
+    public WebClient webClient(HttpClient httpClient) {
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient)).build();
+    }
+}
+
+package com.example;
+
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class SimpleReactiveRequestInterceptor implements ReactiveRequestInterceptor {
+
+    @Override
+    public Mono<ServerWebExchange> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        // Update the request URI
+        String newUri = exchange.getRequest().getURI().toString() + "/updated";
+        ServerWebExchange updatedRequest = exchange.mutate().request(exchange.getRequest().mutate().uri(newUri).build()).build();
+
+        // Update the request path
+        String newPath = updatedRequest.getRequest().getPath() + "/updated";
+        updatedRequest = updatedRequest.mutate().request(updatedRequest.getRequest().mutate().path(newPath).build()).build();
+
+        // Update the request query parameters
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("name", "John Doe");
+        queryParams.put("age", "30");
+        updatedRequest = updatedRequest.mutate().queryParams(queryParams).build();
 
         // Update the request payload
-        Mono<String> requestBody = request.bodyToMono(String.class);
+        Mono<String> requestBody = updatedRequest.getRequest().getBodyToMono(String.class);
         requestBody = requestBody.map(body -> body.replace("old", "new"));
 
         // Wrap the request object
-        updatedRequest = updatedRequest.body(requestBody);
+        updatedRequest = updatedRequest.mutate().request(updatedRequest.getRequest().mutate().body(requestBody).build()).build();
 
         // Continue processing the request
-        return next.filter(updatedRequest);
-}
-
-public class ReactiveResponseInterceptor implements HandlerFilterFunction<ServerRequest, ServerResponse> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ReactiveResponseInterceptor.class);
-
-    @Override
-    public Mono<ServerResponse> filter(ServerRequest request, HandlerFilterFunction<ServerRequest, ServerResponse> next) {
-
-        // Continue processing the request
-        return next.filter(request).map(response -> {
-
-            // Update the response payload
-            Mono<String> responseBody = response.bodyToMono(String.class);
-            responseBody = responseBody.map(body -> body.replace("old", "new"));
-
-            // Wrap the response object
-            ServerResponse updatedResponse = ServerResponse.create(response.statusCode(), response.headers(), responseBody);
-
-            return updatedResponse;
-        });
+        return chain.filter(updatedRequest);
     }
 }
 
-import org.springframework.web.filter.OncePerRequestFilter;
+
+package com.example;
+
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class SimpleReactiveResponseInterceptor implements ReactiveResponseInterceptor {
+
+    @Override
+    public Mono<ServerWebExchange> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        // Update the response payload
+        Mono<String> responseBody = exchange.getResponse().getBodyToMono(String.class);
+        responseBody = responseBody.map(body -> body.replace("old", "new"));
+
+        // Wrap the response object
+        ServerWebExchange updatedResponse = exchange.mutate().response(exchange.getResponse().mutate().body(responseBody).build()).build();
+
+        // Continue processing the request
+        return chain.filter(updatedResponse);
+    }
+}
+
+
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.web.server.ServerWebExchange;
 
-public class RequestResponseFilter extends OncePerRequestFilter {
+public class SimpleReactiveRequestInterceptorTest {
 
-    private final ReactiveRequestInterceptor requestInterceptor;
-    private final ReactiveResponseInterceptor responseInterceptor;
+    private final SimpleReactiveRequestInterceptor interceptor = new SimpleReactiveRequestInterceptor();
 
-    public RequestResponseFilter(ReactiveRequestInterceptor requestInterceptor, ReactiveResponseInterceptor responseInterceptor) {
-        this.requestInterceptor = requestInterceptor;
-        this.responseInterceptor = responseInterceptor;
+    @Test
+    public void testFilter_withValidRequest_shouldUpdateRequest() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getRequest().getURI().toString()).thenReturn("/api/v1/users/1");
+
+        // Act
+        ServerWebExchange updatedRequest = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedRequest.getRequest().getURI().toString()).isEqualTo("/api/v1/users/1/updated");
+        assertThat(updatedRequest.getRequest().getPath()).isEqualTo("/users/1/updated");
+        assertThat(updatedRequest.getRequest().getQueryParams()).containsEntry("name", "John Doe");
+        assertThat(updatedRequest.getRequest().getQueryParams()).containsEntry("age", "30");
+        assertThat(updatedRequest.getRequest().getBody().toMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30}");
     }
 
-    @Override
-    protected Mono<Void> filterInternal(ServerWebExchange exchange, Chain chain) {
-        return requestInterceptor.filter(exchange, chain)
-                .then(responseInterceptor.filter(exchange, chain));
+    @Test
+    public void testFilter_withInvalidRequest_shouldReturnOriginalRequest() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getRequest().getURI().toString()).thenReturn("/invalid/uri");
+
+        // Act
+        ServerWebExchange updatedRequest = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedRequest).isSameAs(exchange);
+    }
+	
+	 @Test
+    public void testFilter_withPostRequest_shouldUpdateRequest() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getRequest().getMethod()).thenReturn(HttpMethod.POST);
+        when(exchange.getRequest().getBody().toMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30}");
+
+        // Act
+        ServerWebExchange updatedRequest = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedRequest.getRequest().getBody().toMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30, \"updated\": true}");
+    }
+
+    @Test
+    public void testFilter_withPutRequest_shouldUpdateRequest() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getRequest().getMethod()).thenReturn(HttpMethod.PUT);
+        when(exchange.getRequest().getBody().toMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30}");
+
+        // Act
+        ServerWebExchange updatedRequest = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedRequest.getRequest().getBody().toMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30, \"updated\": true}");
+    }
+
+    @Test
+    public void testFilter_withDeleteRequest_shouldUpdateRequest() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getRequest().getMethod()).thenReturn(HttpMethod.DELETE);
+
+        // Act
+        ServerWebExchange updatedRequest = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedRequest).isSameAs(exchange);
+    }
+
+    @Test
+    public void testFilter_withRequestHeader_shouldUpdateRequestHeader() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getRequest().getHeaders().getFirst("Authorization")).thenReturn("Bearer my-token");
+
+        // Act
+        ServerWebExchange updatedRequest = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedRequest.getRequest().getHeaders().getFirst("Authorization")).isEqualTo("Bearer my-updated-token");
+    }
+
+    @Test
+    public void testFilter_withRequestBody_shouldUpdateRequestBody() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getRequest().getBody().toMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30}");
+
+        // Act
+        ServerWebExchange updatedRequest = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedRequest.getRequest().getBody().toMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30, \"updated\": true}");
     }
 }
 
-import org.springframework.boot.web.reactive.filter.WebFilterChain;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
-//Once we registered the RequestResponseFilter, it will be executed before all other filters, ensuring that the request and response interceptors are        // executed before everything else.
-@Configuration
-public class WebFluxConfig implements WebFluxConfigurer {
+package com.example;
 
-    private final ReactiveRequestInterceptor requestInterceptor;
-    private final ReactiveResponseInterceptor responseInterceptor;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-    public WebFluxConfig(ReactiveRequestInterceptor requestInterceptor, ReactiveResponseInterceptor responseInterceptor) {
-        this.requestInterceptor = requestInterceptor;
-        this.responseInterceptor = responseInterceptor;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.web.server.ServerWebExchange;
+
+public class SimpleReactiveResponseInterceptorTest {
+
+    private final SimpleReactiveResponseInterceptor interceptor = new SimpleReactiveResponseInterceptor();
+
+    @Test
+    public void testFilter_withValidResponse_shouldUpdateResponse() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getResponse().getBodyToMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30}");
+
+        // Act
+        ServerWebExchange updatedResponse = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedResponse.getResponse().getBodyToMono().block()).isEqualTo("{\"name\": \"John Doe\", \"age\": 30, \"updated\": true}");
     }
 
-    @Override
-    public void addFilters(WebFilterRegistry registry) {
-        registry.addFilterFirst(new RequestResponseFilter(requestInterceptor, responseInterceptor));
+    @Test
+    public void testFilter_withInvalidResponse_shouldReturnOriginalResponse() {
+        // Arrange
+        ServerWebExchange exchange = mock(ServerWebExchange.class);
+        when(exchange.getResponse().getBodyToMono().block()).isNull();
+
+        // Act
+        ServerWebExchange updatedResponse = interceptor.filter(exchange, mock(WebFilterChain.class)).block();
+
+        // Assert
+        assertThat(updatedResponse).isSameAs(exchange);
     }
 }
+
+
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.reactive.server.WebTestClient.BodySpec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@TestPropertySource(properties = "spring.web.reactive.filter.add=requestResponseFilter")
+public class WebFluxIntegrationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testRequestResponseFilter_withValidRequest_shouldUpdateRequestAndResponse() {
+        // Act
+        BodySpec<String> response = webTestClient
+                .get()
+                .uri("/api/v1/users/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class);
+
+        // Assert
+        assertThat(response.extract().body().block()).contains("John Doe").contains("30").contains("updated");
+    }
+
+    @Test
+    public void testRequestResponseFilter_withInvalidRequest_shouldReturn404() {
+        // Act
+        webTestClient
+                .get()
+                .uri("/invalid/uri")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+}
+
