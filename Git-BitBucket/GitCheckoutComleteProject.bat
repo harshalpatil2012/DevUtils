b1ddb6ed9aba7bc69c2fc056a0317a3895a62eed
Harshal Patil <harshalpatil2012@users.noreply.github.com>
2024-01-05 14:14:17 +0530

    Update GitCheckoutComleteProject.bat

--- Git-BitBucket/GitCheckoutComleteProject.bat
+++ Git-BitBucket/GitCheckoutComleteProject.bat
@@ -21,6 +21,9 @@ for /L %%i in (0,1,100) do (
 )
 :break_loop
 
+rem Create or clear the error log file
+type nul > error_log.txt
+
 rem Iterate through the array
 for /L %%i in (0,1,%bitbucket_url_branch_map_count%) do (
     set "line=!bitbucket_url_branch_map[%%i]!"
@@ -32,16 +35,16 @@ for /L %%i in (0,1,%bitbucket_url_branch_map_count%) do (
         set "repo_path=!DESTINATION_DIR!\!repo_name!"
 
         if exist "!repo_path!" (
-            cd "!repo_path!" || ( echo Error: Unable to navigate to !repo_path! folder & exit /B 1 )
-            git pull origin "!branch!" || ( echo Error: Unable to pull latest changes in !repo_path! & exit /B 1 )
+            cd "!repo_path!" || ( echo Error: Unable to navigate to !repo_path! folder & echo Error: Unable to navigate to !repo_path! folder >> error_log.txt & exit /B 1 )
+            git pull origin "!branch!" || ( echo Error: Unable to pull latest changes in !repo_path! & echo Error: Unable to pull latest changes in !repo_path! >> error_log.txt & exit /B 1 )
         ) else (
-            git clone --branch "!branch!" --depth 1 "!bitbucket_url!" "!repo_path!" || ( echo Error: Unable to clone !bitbucket_url! & exit /B 1 )
+            git clone --branch "!branch!" --depth 1 "!bitbucket_url!" "!repo_path!" || ( echo Error: Unable to clone !bitbucket_url! & echo Error: Unable to clone !bitbucket_url! >> error_log.txt & exit /B 1 )
             echo Cloned !bitbucket_url! to !repo_path!
         )
 
         echo Checked out/pulled !branch! in !repo_path!
 
-        cd .. || ( echo Error: Unable to navigate back to the original directory & exit /B 1 )
+        cd .. || ( echo Error: Unable to navigate back to the original directory & echo Error: Unable to navigate back to the original directory >> error_log.txt & exit /B 1 )
     )
 )
 
