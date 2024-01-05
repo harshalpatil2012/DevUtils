4961149b94248ef31889580ede96b651ba16bd89
Harshal Patil <harshalpatil2012@users.noreply.github.com>
2024-01-05 13:02:08 +0530

    updated script

--- Git-BitBucket/GitCheckoutComleteProject.bat
+++ Git-BitBucket/GitCheckoutComleteProject.bat
@@ -3,22 +3,16 @@
 # Set the destination directory where repositories will be cloned
 DESTINATION_DIR="/path/to/destination"
 
-# Define a map for repositories and branches
-declare -A repo_branch_map=(
-    ["repo1"]="master"
-    ["repo2"]="develop"
-    ["repo3"]="feature-branch"
+# Define Bitbucket URLs and associated branches using a map
+declare -A bitbucket_url_branch_map=(
+    ["https://bitbucket.org/user/repo1.git"]="master"
+    ["https://bitbucket.org/user/repo2.git"]="develop"
+    ["https://bitbucket.org/user/repo3.git"]="feature-branch"
 )
 
-# Define Bitbucket URLs
-bitbucket_urls=("https://bitbucket.org/user/repo1.git" "https://bitbucket.org/user/repo2.git" "https://bitbucket.org/user/repo3.git")
-
-IFS=',' read -ra url_array <<< "$bitbucket_urls"
-
-# Loop through each repository in the map
-for repo in "${!repo_branch_map[@]}"; do
-    branch="${repo_branch_map[$repo]}"
-    bitbucket_url="${url_array[i]}"
+# Loop through each Bitbucket URL in the map
+for bitbucket_url in "${!bitbucket_url_branch_map[@]}"; do
+    branch="${bitbucket_url_branch_map[$bitbucket_url]}"
 
     # Extract repository name from the Bitbucket URL
     repo_name=$(basename "$bitbucket_url" | sed 's/\.git$//')
