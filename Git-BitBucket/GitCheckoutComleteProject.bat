e90f3d4929d1b87e345b6fa87daa63749f8fcf5d
Harshal Patil <harshalpatil2012@users.noreply.github.com>
2024-01-05 13:10:34 +0530

    Update GitCheckoutComleteProject.bat

--- Git-BitBucket/GitCheckoutComleteProject.bat
+++ Git-BitBucket/GitCheckoutComleteProject.bat
@@ -5,8 +5,8 @@ DESTINATION_DIR="/path/to/destination"
 
 # Define Bitbucket URLs and associated branches using a map
 declare -A bitbucket_url_branch_map=(
-    ["https://bitbucket.org/user/repo1.git"]="master"
-    ["https://bitbucket.org/user/repo2.git"]="develop"
+    ["https://bitbucket.org/user/repo1.git"]="master" \
+    ["https://bitbucket.org/user/repo2.git"]="develop" \
     ["https://bitbucket.org/user/repo3.git"]="feature-branch"
 )
 
@@ -38,3 +38,34 @@ for bitbucket_url in "${!bitbucket_url_branch_map[@]}"; do
     # Move back to the original directory
     cd - || { echo "Error: Unable to navigate back to the original directory"; exit 1; }
 done
+
+
+
+@echo off
+
+set DESTINATION_DIR=C:\path\to\destination  :: Replace with your desired destination
+
+set bitbucket_url_branch_map[0]=https://bitbucket.org/user/repo1.git master
+set bitbucket_url_branch_map[1]=https://bitbucket.org/user/repo2.git develop
+set bitbucket_url_branch_map[2]=https://bitbucket.org/user/repo3.git feature-branch
+
+for /F "tokens=1,2 delims==" %%a in ('set bitbucket_url_branch_map[') do (
+    set "bitbucket_url=%%a"
+    set "branch=%%b"
+
+    for %%p in ("%bitbucket_url%") do set "repo_name=%%~np"
+    set "repo_path=%DESTINATION_DIR%\%repo_name%"
+
+    if not exist "%repo_path%" (
+        git clone "%bitbucket_url%" "%repo_path%" || ( echo Error: Unable to clone %bitbucket_url% & exit /B 1 )
+        echo Cloned %bitbucket_url% to %repo_path%
+    )
+
+    cd "%repo_path%" || ( echo Error: Unable to navigate to %repo_path% folder & exit /B 1 )
+
+    git checkout "%branch%" || ( echo Error: Unable to checkout %branch% in %repo_path% & exit /B 1 )
+
+    echo Checked out %branch% in %repo_path%
+
+    cd .. || ( echo Error: Unable to navigate back to the original directory & exit /B 1 )
+)
