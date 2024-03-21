51b0e065981ea0a9f1d1f36cfa2b7461c00666e3
Harshal Patil <harshalpatil2012@users.noreply.github.com>
2024-03-21 11:11:05 +0530

    Update database-ddl-copynew.sql

--- Database/database-ddl-copy.sql
+++ Database/database-ddl-copy.sql
@@ -6,7 +6,7 @@ BEGIN
          file_handle UTL_FILE.FILE_TYPE;
       BEGIN
          ddl := DBMS_METADATA.get_ddl('TABLE',t.table_name);
-         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', t.table_name || '.sql', 'w');
+         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', 'TABLE_' || t.table_name || '.sql', 'w');
          UTL_FILE.put_line(file_handle, ddl);
          UTL_FILE.fclose(file_handle);
       END;
@@ -22,7 +22,7 @@ BEGIN
          file_handle UTL_FILE.FILE_TYPE;
       BEGIN
          ddl := DBMS_METADATA.get_ddl('VIEW',v.view_name);
-         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', v.view_name || '.sql', 'w');
+         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', 'VIEW_' || v.view_name || '.sql', 'w');
          UTL_FILE.put_line(file_handle, ddl);
          UTL_FILE.fclose(file_handle);
       END;
@@ -38,7 +38,39 @@ BEGIN
          file_handle UTL_FILE.FILE_TYPE;
       BEGIN
          ddl := DBMS_METADATA.get_ddl('PROCEDURE',p.object_name);
-         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', p.object_name || '.sql', 'w');
+         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', 'PROCEDURE_' || p.object_name || '.sql', 'w');
+         UTL_FILE.put_line(file_handle, ddl);
+         UTL_FILE.fclose(file_handle);
+      END;
+   END LOOP;
+END;
+/
+
+-- For Functions
+BEGIN
+   FOR f IN (SELECT object_name FROM user_procedures WHERE object_type = 'FUNCTION') LOOP
+      DECLARE
+         ddl CLOB;
+         file_handle UTL_FILE.FILE_TYPE;
+      BEGIN
+         ddl := DBMS_METADATA.get_ddl('FUNCTION',f.object_name);
+         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', 'FUNCTION_' || f.object_name || '.sql', 'w');
+         UTL_FILE.put_line(file_handle, ddl);
+         UTL_FILE.fclose(file_handle);
+      END;
+   END LOOP;
+END;
+/
+
+-- For Packages
+BEGIN
+   FOR p IN (SELECT object_name FROM user_procedures WHERE object_type = 'PACKAGE') LOOP
+      DECLARE
+         ddl CLOB;
+         file_handle UTL_FILE.FILE_TYPE;
+      BEGIN
+         ddl := DBMS_METADATA.get_ddl('PACKAGE',p.object_name);
+         file_handle := UTL_FILE.fopen('DIRECTORY_PATH', 'PACKAGE_' || p.object_name || '.sql', 'w');
          UTL_FILE.put_line(file_handle, ddl);
          UTL_FILE.fclose(file_handle);
       END;
